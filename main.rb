56ca4614b0f152bd033d4fb4026b6264fac19e8a
Patricio Bruna <pbruna@gmail.com>
2014-05-03 21:38:02 -0400

    Testing on heroku

--- main.rb
+++ main.rb
@@ -22,33 +22,43 @@ Trello.configure do |config|
   config.member_token = ENV['TRELLO_TOKEN']
 end
 
-hipchat_client = HipChat::Client.new(ENV['HIPCHAT_APIv2_TOKEN'], api_version: "v2")
-scheduler = Rufus::Scheduler.new
-
-scheduler.cron '35 21 * * * America/Santiago' do
-  board = Trello::Board.find(ENV['TRELLO_BOARD'])
-  members_id_hash = Hash.new([])
-
-  lists = board.lists.keep_if {|list| BOARD_LISTS.include? list.name }
-
-  lists[0].cards.each do |card|
-    card.member_ids.each do |mid|
-      members_id_hash[mid] |= Array.new
-      members_id_hash[mid] << card.id
-    end
+class Main
   
-  end
+  def self.run
 
-  members_id_hash.each do |key,val|
-    member = Trello::Member.find(key)
-    message = "#{member.full_name} para hoy tienes las siguientes tareas:\n\n"
-    val.each do |card|
-      card = Trello::Card.find(card)
-      message << "- #{card.name} - #{card.short_url}\n"
-    end
+    hipchat_client = HipChat::Client.new(ENV['HIPCHAT_APIv2_TOKEN'], api_version: "v2")
+    scheduler = Rufus::Scheduler.new
 
-    message << "\n(gangnamstyle)"
-    hipchat_client.user(TRELLO_2_HIPCHAT[member.username]).send(message)
+    scheduler.cron '39 21 * * * America/Santiago' do
+      board = Trello::Board.find(ENV['TRELLO_BOARD'])
+      members_id_hash = Hash.new([])
+
+      lists = board.lists.keep_if {|list| BOARD_LISTS.include? list.name }
+
+      lists[0].cards.each do |card|
+        card.member_ids.each do |mid|
+          members_id_hash[mid] |= Array.new
+          members_id_hash[mid] << card.id
+        end
   
+      end
+
+      members_id_hash.each do |key,val|
+        member = Trello::Member.find(key)
+        message = "#{member.full_name} para hoy tienes las siguientes tareas:\n\n"
+        val.each do |card|
+          card = Trello::Card.find(card)
+          message << "- #{card.name} - #{card.short_url}\n"
+        end
+
+        message << "\n(gangnamstyle)"
+        hipchat_client.user(TRELLO_2_HIPCHAT[member.username]).send(message)
+  
+      end
+    end
   end
+end
+
+if __FILE__ == $0
+  Main.run
 end
